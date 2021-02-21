25e012a1155b8c1ca196133146ded7794d0b0d5f
Bryner20 <34035527+Bryner20@users.noreply.github.com>
2021-02-21 12:28:03 -0500

    selfportrait

---
+++ selfportrait/sketch.js
@@ -0,0 +1,24 @@
+
+function setup() {
+	createCanvas(740, 460);
+}
+function draw() {
+	background('lightgreen');
+    
+    fill(255, 183, 127);
+	ellipse(367, 155, 195, 195);
+    
+    fill('grey');
+	circle(400, 150, 30);
+	fill(0);
+	circle(340, 150, 30);
+
+    fill('white')	
+	rect(410, 65, 65, 55, 25, 5, 25, 20);
+	fill('white')
+	rect(265, 65, 65, 55, 5, 25, 25, 20);
+      
+	line(355, 200, 385, 210);
+	line(415, 150, 465, 150);
+
+}
